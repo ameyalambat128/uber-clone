231f6f6511c20ab184a6e5ba098da4ef8d0257a1
Ameya Lambat <ameyalambat@gmail.com>
2021-12-12 17:18:07 -0700

    fix: SafeAreaView was not working on android installed react-native-safe-area-context to fix this

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import tw from "tailwind-react-native-classnames";
 
 const HomeScreen = () => {
