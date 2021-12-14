9a4918b4b756de6db0e6b74da3b36e1e3f897fc2
Ameya Lambat <ameyalambat@gmail.com>
2021-12-14 03:17:07 -0700

    feat: Divided the mapscreen page into 2 parts used native maps on the top half

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -5,8 +5,12 @@ import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5 `}>
@@ -27,7 +31,16 @@ const HomeScreen = () => {
             container: { flex: 0 },
             textInput: { fontSize: 18 },
           }}
-          onPress={(data, details = null) => {}}
+          returnKeyType={"search"}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+            dispatch(setDestination(null));
+          }}
           fetchDetails={true}
           enablePoweredByContainer={false}
           minLength={2}
--- screens/MapScreen.jsx
+++ screens/MapScreen.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import tw from "tailwind-react-native-classnames";
+import Map from "../components/Map";
+import MapView, { Marker } from "react-native-maps";
 
 const MapScreen = () => {
   return (
     <View>
       <Text>here is MapScreen</Text>
+      <View style={tw`h-1/2`}>
+        <Map />
+      </View>
+      <View style={tw`h-1/2`}></View>
     </View>
   );
 };
